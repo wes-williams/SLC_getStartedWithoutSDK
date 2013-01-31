a33f43288457331413e7b9b7ebbcd09fdfe94230
Wes Williams <wwwestech@gmail.com>
2013-01-30 22:52:32 -0700

    initial/complete commit

---
+++ README.md
@@ -0,0 +1,6 @@
+slcApp_common
+==============
+
+SLC Example App using common frameworks
+
+Get Started: Just change the bogus client id and token
---
+++ example.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var passport = require('passport')
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var request = require('request');
+
+var handleRequest = function(user, options, callback) {
+  request({ 
+        'uri' : 'https://api.sandbox.slcedu.org/api/rest/v1' + options.uri,
+        'body' : options.body,
+        'method' : options.method,
+        'headers' :  { 'Authorization': 'Bearer ' + user.accessToken, 
+	               'Content-Type': 'application/json'}
+  }, 
+  function(error, response, body) {
+    if(error) {
+      callback(user,null);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      callback(user,JSON.parse(body));
+    } else {
+      callback(user,response.statusCode);
+    }
+  });
+};
+
+/////////////////////////////////
+// Start Persistent Session Code
+// Note : Use a real store
+/////////////////////////////////
+var users = [];
+var findUser = function(request) { return users[request.session.passport.user]; };
+
+passport.serializeUser(function(user, done) {
+  users[user.user_id] = user;
+  done(null, user.user_id);
+});
+
+passport.deserializeUser(function(id, done) {
+  var user = users[id];
+  done(null, user);
+});
+/////////////////////////////////
+// End Persistent Session Code
+/////////////////////////////////
+
+passport.use('slc', new OAuth2Strategy({
+    authorizationURL: 'https://api.sandbox.slcedu.org/api/oauth/authorize',
+    tokenURL: 'https://api.sandbox.slcedu.org/api/oauth/token?grant_type=authorization_code',
+    clientID: 'asdF34gsd',
+    clientSecret: 'T46S1sdfkjdfgui4Ffg3gedfqdfORg2d436',
+    callbackURL: 'http://127.0.0.1:3000/auth/slc/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+    var fakeUser = { 'accessToken' : accessToken }; 
+    var options =  { 'method' : 'GET', 
+                     'uri' : '/system/session/check' };
+
+    var callback = function(user, data) {
+      var fullUser = null;
+      if(data  && data.user_id) { 
+        fullUser = data;
+        fullUser.accessToken = user.accessToken;
+      } 
+      done(null, fullUser);
+    };
+
+    handleRequest(fakeUser,options,callback);  
+  }
+));
+
+
+// thanks slc sdk...
+function requireToken() {
+  return function(req, res, next) {
+     if (req.session.passport.user &&
+         findUser(req)) {
+       next();
+     }
+     else {
+       res.redirect('/login');
+     }
+   }
+}
+
+var app = express();
+
+app.configure(function() {
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'akdjfh34gliub97gel' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+});
+
+app.get('/auth/slc', passport.authenticate('slc'));
+
+app.get('/auth/slc/callback', 
+  passport.authenticate('slc', { successRedirect: '/',
+                                 failureRedirect: '/login' }));
+
+app.get('/login', function(req, res){
+  res.render('login.jade',{ 'title' : 'My App - Login' });
+});
+
+app.get('/logout', requireToken(), function(req, res) {
+  handleRequest(findUser(req),
+                { 'method' : 'GET', 'uri' : '/system/session/logout' },
+		function(user, data) {
+                  res.redirect('/login');
+		});
+});
+
+app.get('/', requireToken(), function(req, res) {
+  res.redirect('/dashboard'); 
+});
+
+app.get('/dashboard', requireToken(), function(req, res) {
+  res.render('index.jade',{ 'title' : 'My App' }); 
+});
+
+app.get('/schools', requireToken(), function(req, res) {
+  handleRequest(findUser(req),
+                { 'method' : 'GET', 'uri' : '/schools' },
+		function(user, data) {
+                  res.render('schools.jade',{ 'title' : 'My App - Schools', 'schools' : data });
+		});
+});
+
+app.get('/students', requireToken(), function(req, res) {
+  handleRequest(findUser(req),
+                { 'method' : 'GET', 'uri' : '/students' },
+		function(user, data) {
+                  res.render('students.jade',{ 'title' : 'My App - Students', 'students' : data });
+		});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
