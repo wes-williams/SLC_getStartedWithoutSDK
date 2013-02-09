027d99114df32bb522ddfcd84d0d4d3e2482ba5b
Wes Williams <wwwestech@gmail.com>
2013-02-09 10:45:37 -0700

    change slc urls to inbloom

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 var appConfig = {
   'port' : 3000,
   'sessionSecret' : '**PutSecretSessionDataHere**',
-  'requestUrl' : 'https://api.sandbox.slcedu.org/api/rest/v1',
+  'requestUrl' : 'https://api.sandbox.inbloom.org/api/rest/v1',
   'oauth' : {
-    'authorizationUrl' : 'https://api.sandbox.slcedu.org/api/oauth/authorize',
-    'tokenUrl' : 'https://api.sandbox.slcedu.org/api/oauth/token?grant_type=authorization_code', 
+    'authorizationUrl' : 'https://api.sandbox.inbloom.org/api/oauth/authorize',
+    'tokenUrl' : 'https://api.sandbox.inbloom.org/api/oauth/token?grant_type=authorization_code', 
     'clientId' : '**PutYourSlcClientIdHere**',
     'clientSecret' : '**PutYourSlcSecretHere**',
     'callbackUrl' : 'http://127.0.0.1:3000/auth/slc/callback'
